cba4537aa337494d53d3884fa8ea6f138c2caa75
nnamdi_ajah <nnamdi.ajah@simplifa.de>
2022-04-11 07:30:18 +0200

    implement exclude delete

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
+#include "ConfParser.h"
 #include "DeletePolicy.h"
 #include "DirControlPolicy.h"
-#include "ConfParser.h"
 
 #include <filesystem>
+#include <fstream>
 #include <future>
 #include <iostream>
-#include <fstream>
 #include <sstream>
 
 using std::cin;
@@ -16,6 +16,7 @@ struct dirPolicy {
   std::unique_ptr<string> path;
   std::unique_ptr<DirControlPolicy> controlPolicy;
   std::unique_ptr<DeletePolicy> cleanUpPolicy;
+  std::unique_ptr<vector<string>> excludeFiles;
 };
 
 struct inputConf {
@@ -32,7 +33,7 @@ void ReadConf(vector<inputConf> &confs) {
 
   std::ifstream stream(ConfParser::confFile);
   if (stream.is_open()) {
-    cout << "confs path exists"<< std::endl;
+    cout << "confs path exists" << std::endl;
     string key;
     string line;
     string delimiter;
@@ -44,7 +45,7 @@ void ReadConf(vector<inputConf> &confs) {
     string deletePolicyType;
     vector<string> deletePolicyValue;
     string buffer;
-    inputConf* configuration = new inputConf;
+    inputConf *configuration = new inputConf;
     while (std::getline(stream, line)) {
       pathValue = ""; // prevent concatenation of multiple paths
       std::replace(line.begin(), line.end(), '"', ' ');
@@ -53,7 +54,7 @@ void ReadConf(vector<inputConf> &confs) {
       std::istringstream linestream(line);
       linestream >> key;
       if (ignore.find(key) != std::string::npos) {
-        cout << "Key is... "<< key << std::endl;
+        cout << "Key is... " << key << std::endl;
         continue;
       }
       if (key == "}") {
@@ -61,40 +62,41 @@ void ReadConf(vector<inputConf> &confs) {
         configuration = new inputConf;
       }
       if (key == ConfParser::pathKey) {
-        while (linestream >> buffer){
-          if (pathValue.empty()){
+        while (linestream >> buffer) {
+          if (pathValue.empty()) {
             pathValue = buffer;
-          }
-          else { // take care of empty space in folder names
+          } else { // take care of empty space in folder names
             pathValue += " ";
             pathValue += buffer;
           }
         }
         configuration->path = pathValue;
-        cout << "pathValue is: "<< pathValue << std::endl;
+        cout << "pathValue is: " << pathValue << std::endl;
       }
       if (key == ConfParser::controlPolicyKey) {
         linestream >> controlPolicyType >> controlPolicyValue;
-        cout << "controlPolicyType is: "<< controlPolicyType << std::endl;
-        cout << "controlPolicyValue is: "<< controlPolicyValue << std::endl;
-        if (controlPolicyType == ConfParser::maxSizeInMb){
+        cout << "controlPolicyType is: " << controlPolicyType << std::endl;
+        cout << "controlPolicyValue is: " << controlPolicyValue << std::endl;
+        if (controlPolicyType == ConfParser::maxSizeInMb) {
           configuration->dirControlPolicy = controlPolicyType::max_size_in_mb;
-        } else if (controlPolicyType == ConfParser::maxNumOfContent){
-          configuration->dirControlPolicy = controlPolicyType::max_num_of_content;
+        } else if (controlPolicyType == ConfParser::maxNumOfContent) {
+          configuration->dirControlPolicy =
+              controlPolicyType::max_num_of_content;
         }
         configuration->dirControlPolicyValue = controlPolicyValue;
       }
       if (key == ConfParser::deletePolicyKey) {
         linestream >> deletePolicyType;
-        cout << "deletePolicyType is: "<< deletePolicyType << std::endl;
+        cout << "deletePolicyType is: " << deletePolicyType << std::endl;
         if (deletePolicyType == ConfParser::delAll) {
           configuration->dirDeletepolicy = deletePolicyType::ALL;
         } else if (deletePolicyType == ConfParser::excludeSome) {
           configuration->dirDeletepolicy = deletePolicyType::exclude;
-        }
-        while (linestream >> buffer) {
-          deletePolicyValue.emplace_back(buffer);
-          cout << "buffer is: "<< buffer << std::endl;
+          while (linestream >> buffer) {
+            deletePolicyValue.emplace_back(buffer);
+            cout << "buffer is: " << buffer << std::endl;
+          }
+          configuration->dirExcludeSome = deletePolicyValue;
         }
       }
     }
@@ -116,12 +118,15 @@ int main() {
   ReadConf(confs);
 
   vector<dirPolicy> directories;
-  for (auto &configuration : confs)
-  {
+  for (auto &configuration : confs) {
     if (fs::is_directory(configuration.path)) {
-      DirControlPolicy controlPolicy(configuration.dirControlPolicy, configuration.dirControlPolicyValue);
-      DeletePolicy cleanUpPolicy(configuration.dirDeletepolicy);
-
+      DirControlPolicy controlPolicy(configuration.dirControlPolicy,
+                                     configuration.dirControlPolicyValue);
+      DeletePolicy cleanUpPolicy =
+          (configuration.dirDeletepolicy == deletePolicyType::exclude)
+              ? DeletePolicy(configuration.dirDeletepolicy,
+                             configuration.dirExcludeSome)
+              : DeletePolicy(configuration.dirDeletepolicy);
       dirPolicy testEvery;
       testEvery.path = std::make_unique<string>(std::move(configuration.path));
       testEvery.controlPolicy =
@@ -129,10 +134,9 @@ int main() {
       testEvery.cleanUpPolicy =
           std::make_unique<DeletePolicy>(std::move(cleanUpPolicy));
       directories.emplace_back(std::move(testEvery));
-    }
-    else
-    {
-      cout << "Path ,," << configuration.path << "´´ does not exists" << std::endl;
+    } else {
+      cout << "Path ,," << configuration.path << "´´ does not exists"
+           << std::endl;
     }
   }
 
@@ -145,7 +149,7 @@ int main() {
           std::cout << "Directory ,," << (*directory.path)
                     << "´´ policy is breached, cleaning up... :-)" << std::endl;
 
-          // clean up in separate thread 
+          // clean up in separate thread
           futures.emplace_back(std::async(std::launch::async, [&directory]() {
             directory.cleanUpPolicy->CleanUp(*directory.path);
           }));
@@ -153,7 +157,7 @@ int main() {
       }
       // wait for tasks to complete
       for (const std::future<void> &ftr : futures)
-          ftr.wait();
+        ftr.wait();
       std::this_thread::sleep_for(std::chrono::milliseconds(300));
     }
   }
